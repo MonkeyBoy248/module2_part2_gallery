c4054973c0c57867facbc3957200af93e1954b35
Kirill Temnyi <kirik201100@gmail.com>
2022-03-09 18:28:26 +0300

    feat: add endpoint for authentication api
    create endpoint that returns token object after user validity check
    
    MODULE-2-PART-2

---
+++ src/server/components/authentication/authenticationAuthorizedList.ts
@@ -0,0 +1,10 @@
+export interface Authorized {
+  [email: string]: string;
+}
+
+export const authorized_users: Authorized = {
+  'asergeev@flo.team':'jgF5tn4F',
+  'vkotikov@flo.team':'po3FGas8',
+  'tpupkin@flo.team':'tpupkin@flo.team'
+}
+
