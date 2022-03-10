392741b235bd323516bf119e0414f462ad19095b
Kirill Temnyi <kirik201100@mail.ru>
2022-03-10 18:22:06 +0300

    refactor: merge all client module files into the clientVariables file
    MODULE-2-PART-2

---
+++ src/client/pages/modules/clientVariables.ts
@@ -0,0 +1,66 @@
+const port = 5000;
+const authenticationServerUrl = `http://localhost:${port}/authentication`;
+const galleryServerUrl = `http://localhost:${port}/gallery`;
+const galleryUrl = `index.html`;
+const loginUrl = `authentication.html`;
+const currentUrl = new URL(window.location.href);
+
+interface CustomEventListener {
+  target: HTMLElement | Window | Document;
+  type: string; 
+  handler: EventListenerOrEventListenerObject;
+}
+
+function removeEventListeners (listeners: CustomEventListener[]) {
+  for (let listener of listeners) {
+    listener.target.removeEventListener(listener.type, listener.handler);
+  }
+}
+
+interface User {
+  email: string;
+  password: string;
+}
+
+const tokenKey: string = 'token';
+
+interface Token {
+  token: string;
+  timestamp?: number;
+}
+
+interface AuthenticationErrorMessage {
+  errorMessage: string;
+}
+
+function getToken (): Token {
+  return JSON.parse(localStorage.getItem(tokenKey) || 'null');
+}
+
+function getTokenTimestamp (): number {
+  return JSON.parse(localStorage.getItem(tokenKey) || 'null');
+}
+
+function setToken (token: Token): void {
+  token.timestamp = Date.now();
+  localStorage.setItem(tokenKey, JSON.stringify(token));
+}
+
+function deleteToken (): void {
+  if (Date.now() - getTokenTimestamp() >= 600000) {
+    localStorage.removeItem(tokenKey);
+  }
+}
+
+function redirectToTheGalleryPage () {
+  if (!currentUrl.searchParams.get('currentPage')) {
+    window.location.replace(`${galleryUrl}?page=1`)
+  } else {
+    window.location.replace(`${galleryUrl}?page=${currentPage}`)
+  }
+}
+
+
+
+
+
